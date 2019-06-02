4404c7661f25c2ecde86f14a5b81cc725437b575
AFaust <axel.faust@acosix.org>
2019-06-02 22:27:15 +0200

    Use generated uid + mark TODO in commented config

--- repository/src/main/java/org/aldica/repo/ignite/cache/NodePropertiesValueHolder.java
+++ repository/src/main/java/org/aldica/repo/ignite/cache/NodePropertiesValueHolder.java
@@ -19,7 +19,7 @@ import org.alfresco.util.ParameterCheck;
 public class NodePropertiesValueHolder implements Serializable
 {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -6273084156015809762L;
 
     protected final long[] qnameIds;
 
