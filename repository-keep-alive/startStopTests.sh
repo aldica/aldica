6a3988be820606ef545505077494f1cc56d85eff
Andreas Kring <andreas@magenta.dk>
2020-06-02 17:08:11 +0200

    Fixed a few minor syntactical errors in shell script

--- repository-keep-alive/startStopTests.sh
+++ repository-keep-alive/startStopTests.sh
@@ -8,85 +8,84 @@ set -uo pipefail
 
 mvn clean integration-test -P StartStopTestPreparations
 
-docker-compose -f target\docker\start-stop-test-compose.yml up -d repository01
+docker-compose -f target/docker/start-stop-test-compose.yml up -d repository01
 sleep 15
 
-ticket1=$(`curl --max-time 180 "http://localhost:8180/alfresco/service/api/login?u=admin&pw=admin" 2>&1 | grep -Eo "TICKET_[^<]+"`)
+ticket1=$(curl --max-time 180 "http://localhost:8180/alfresco/service/api/login?u=admin&pw=admin" 2>&1 | grep -Eo "TICKET_[^<]+")
 
 if [[ ! $? -eq 0 ]]
 then
    echo "Error retrieving authentication ticket"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-docker-compose -f target\docker\start-stop-test-compose.yml up -d keepAlive01
+docker-compose -f target/docker/start-stop-test-compose.yml up -d keepAlive01
 sleep 10
 
-docker-compose -f target\docker\start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 2 active nodes"
+docker-compose -f target/docker/start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 2 active nodes"
 
 if [[ ! $? -eq 0 ]]
 then
    echo "Repository01 and KeepAlive01 did not connect"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-docker-compose -f target\docker\start-stop-test-compose.yml stop repository01
+docker-compose -f target/docker/start-stop-test-compose.yml stop repository01
 sleep 10
 
-docker-compose -f target\docker\start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 1 active nodes"
+docker-compose -f target/docker/start-stop-test-compose.yml logs --tail 15 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 1 active nodes"
 
 if [[ ! $? -eq 0 ]]
 then
    echo "KeepAlive01 did not handle disconnect"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-docker-compose -f target\docker\start-stop-test-compose.yml up -d keepAlive02
+docker-compose -f target/docker/start-stop-test-compose.yml up -d keepAlive02
 sleep 10
 
-docker-compose -f target\docker\start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 2 active nodes"
+docker-compose -f target/docker/start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 2 active nodes"
 
 if [[ ! $? -eq 0 ]]
 then
    echo "KeepAlive01 and KeepAlive02 did not connect"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-docker-compose -f target\docker\start-stop-test-compose.yml up -d repository02
+docker-compose -f target/docker/start-stop-test-compose.yml up -d repository02
 sleep 15
 
-ticket2=$(`curl --max-time 180 "http://localhost:8280/alfresco/service/api/login?u=admin&pw=admin" 2>&1 | grep -Eo "TICKET_[^<]+"`)
-
+ticket2=$(curl --max-time 180 "http://localhost:8280/alfresco/service/api/login?u=admin&pw=admin" 2>&1 | grep -Eo "TICKET_[^<]+")
 
 if [[ ! $? -eq 0 ]]
 then
    echo "Error retrieving authentication ticket"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-docker-compose -f target\docker\start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 3 active nodes"
+docker-compose -f target/docker/start-stop-test-compose.yml logs --tail 6 keepAlive01 | grep -o "Ignite instance repositoryGrid currently has 3 active nodes"
 
 if [[ ! $? -eq 0 ]]
 then
    echo "Repository02 and KeepAlive nodes did not connect"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-if [[ ! $ticket1 -eq $ticket2 ]]
+if [ "$ticket1" != "$ticket2" ]
 then
    echo "Authentication ticket was not properly kept alive and replicated to Repository02"
    echo "Original ticket on Repository01: $ticket1"
    echo "New ticket on Repository02: $ticket2"
-   docker-compose -f target\docker\start-stop-test-compose.yml down -v
+   docker-compose -f target/docker/start-stop-test-compose.yml down -v
    exit 1
 fi
 
-docker-compose -f target\docker\start-stop-test-compose.yml down -v
+docker-compose -f target/docker/start-stop-test-compose.yml down -v
 
-echo "Start-Stop tests of KeepAlive and Repository components completed succesfully"
+echo "Start-Stop tests of KeepAlive and Repository components completed successfully"
