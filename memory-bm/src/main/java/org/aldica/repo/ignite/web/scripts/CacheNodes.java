374a3ec1b4c4c012b18bd9d955be80ae57c45ee1
AFaust <axel.faust@acosix.org>
2020-07-04 19:04:25 +0200

    Last post-benchmark changes
    - analysis of unexpected results for aspectsSharedCache
    - optional preloadQNames URL parameter for benchmark to verify limited
      impact of TransactionalCache flaw
    - enable useIdsWhenPossible by default
    - add missing specific cache types for various local/invalidating caches

--- memory-bm/src/main/java/org/aldica/repo/ignite/web/scripts/CacheNodes.java
+++ memory-bm/src/main/java/org/aldica/repo/ignite/web/scripts/CacheNodes.java
@@ -11,13 +11,16 @@ import java.util.function.Function;
 import org.alfresco.repo.batch.BatchProcessWorkProvider;
 import org.alfresco.repo.batch.BatchProcessor;
 import org.alfresco.repo.batch.BatchProcessor.BatchProcessWorker;
+import org.alfresco.repo.domain.qname.QNameDAO;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.cmr.search.ResultSet;
 import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.QName;
 import org.alfresco.service.transaction.TransactionService;
+import org.alfresco.util.Pair;
 import org.alfresco.util.PropertyCheck;
 import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
@@ -37,6 +40,8 @@ public class CacheNodes extends AbstractWebScript implements InitializingBean
 
     protected TransactionService transactionService;
 
+    protected QNameDAO qnameDAO;
+
     protected NodeService nodeService;
 
     protected SearchService searchService;
@@ -49,6 +54,7 @@ public class CacheNodes extends AbstractWebScript implements InitializingBean
     public void afterPropertiesSet()
     {
         PropertyCheck.mandatory(this, "transactionService", this.transactionService);
+        PropertyCheck.mandatory(this, "qnameDAO", this.qnameDAO);
         PropertyCheck.mandatory(this, "nodeService", this.nodeService);
         PropertyCheck.mandatory(this, "searchService", this.searchService);
     }
@@ -60,6 +66,15 @@ public class CacheNodes extends AbstractWebScript implements InitializingBean
     public void setTransactionService(final TransactionService transactionService)
     {
         this.transactionService = transactionService;
+    }
+
+    /**
+     * @param qnameDAO
+     *            the qnameDAO to set
+     */
+    public void setQnameDAO(final QNameDAO qnameDAO)
+    {
+        this.qnameDAO = qnameDAO;
     }
 
     /**
@@ -89,8 +104,31 @@ public class CacheNodes extends AbstractWebScript implements InitializingBean
     {
         final int threads = Integer.parseInt(req.getParameter("threads"));
         final int count = Integer.parseInt(req.getParameter("countNodes"));
+        final boolean preloadQNames = Boolean.parseBoolean(req.getParameter("preloadQNames"));
 
-        final String nameSuffix = "of" + count;
+        // preload custom aspect / property QName instances - Alfresco seems to be unable to stabilise immutableSingletonSharedCache if
+        // loaded in concurrent batch processors, reloading + replacing the same cache entries from DB over and over
+        if (preloadQNames)
+        {
+            this.transactionService.getRetryingTransactionHelper().doInTransaction(() -> {
+                final QName[] toLoad = new QName[] { GenerateNodes.INT_A, GenerateNodes.LONG_B, GenerateNodes.FLOAT_C,
+                        GenerateNodes.DOUBLE_D, GenerateNodes.DATE_E, GenerateNodes.DATE_F, GenerateNodes.NODE_G, GenerateNodes.QNAME_H,
+                        GenerateNodes.LOV_STRING, QName.createQName("http://www.aldica.org/model/mem-bm/1.0", "benchmarkProperties") };
+
+                for (final QName qname : toLoad)
+                {
+                    final Pair<Long, QName> pair = this.qnameDAO.getQName(qname);
+                    if (pair != null)
+                    {
+                        this.qnameDAO.getQName(pair.getFirst());
+                    }
+                }
+
+                return null;
+            }, true);
+        }
+
+        final String nameSuffix = " of " + count;
         this.loadNodes(count, threads, i -> i + nameSuffix);
     }
 
